1313d97648fbfef7d3a338ec96f8371c6704052b
lokesh purigi <plokesh7428@gmail.com>
2022-08-30 23:49:22 +0530

    intro-js

---
+++ indroction-to-js.js
@@ -0,0 +1,33 @@
+function processData(input) {
+    //Enter your code here
+    
+    console.log("Hello World!");
+    console.log(input)
+}
+processData(5)
+
+
+// 3rd ques
+
+let newVariable = "AlmaBetter";
+console.log(newVariable);
+
+// 4th ques
+
+// let number = "777";
+// let integer = 8;
+
+// let v = number + integer;
+// console.log(v);
+
+ let number = "777";
+ let integer = 8;
+ let  inv = "785"
+ let addedinteger;
+ let inn = parseInt(inv);
+ addedinteger = "777" + 8; 
+ console.log(addedinteger);
+ console.log(inn);
+ 
+
+
